ef83d6e8fbf767aad33718b106705ca10632a653
Marvi Gatus <Marvi.Gatus@site.edu.au>
2017-10-09 17:07:12 +0800

    firebase update

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="sign-options">
     <h1>Sign in options</h1>
-    <router-link to="/manualsignin" class="button">Manual Sign In</router-link><br><br>
-    <router-link to="/signup" class="button">Sign Up Here!</router-link>
+    <router-link to="/manualsignin" class="button"><i class="fa fa-sign-in" aria-hidden="true"></i> Manual Sign In</router-link><br>
+    <router-link to="/signup" class="button"><i class="fa fa-pencil-square" aria-hidden="true"></i> Sign Up Here!</router-link>
     <div id="firebaseui-auth-container"></div>
   </div>
 
@@ -31,3 +31,14 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="sass">
+
+  .sign-options a.button
+    width: 220px
+    margin: 0 0 1%
+    box-shadow: 0px 3px 2px rgba(0, 0, 0, 0.38)
+    .fa
+      margin-right: 2px
+
+</style>
